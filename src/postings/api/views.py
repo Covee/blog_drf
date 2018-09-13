25e5221e8aaf90975845fe2a46d4f64c79806ab4
Covee <uniqtop91@gmail.com>
2018-09-13 17:22:30 +0900

    mixin 사용해서 listAPIView에 추가적인 기능 넣어줌 & basic Q 서치

--- src/postings/api/views.py
+++ src/postings/api/views.py
@@ -1,18 +1,29 @@
-from rest_framework import generics
+from django.db.models import Q
+from rest_framework import generics, mixins
 
 from postings.models import BlogPost
 from .serializers import BlogPostSerializer
 
 
-class BlogPostAPIView(generics.CreateAPIView):
+class BlogPostAPIView(mixins.CreateModelMixin, generics.ListAPIView):
 	lookup_field 		= 'pk'
 	serializer_class 	= BlogPostSerializer
 
 	def get_queryset(self):
-		return BlogPost.objects.all()
+		qs = BlogPost.objects.all()
+		query = self.request.GET.get("q")
+		if query is not None:
+			qs = qs.filter(
+					Q(title__icontains=query)|		# Q의 사용은 'or, 즉|' 을 가능하게 해줌
+					Q(content__icontains=query)
+					).distinct()
+		return qs
 
 	def perform_create(self, serializer):	# user가 read_only라서 api창에서 꼭 골라야한다는 에러가 뜨는데, 이 코드로 request.user 받게끔 설정해 줌
 		serializer.save(user=self.request.user)
+
+	def post(self, request, *args, **kwargs):
+		return self.create(request, *args, **kwargs)
 
 
 class BlogPostRudView(generics.RetrieveUpdateDestroyAPIView):
